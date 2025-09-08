f3f86fcf3f794300f61d2d57f17cf6f5618b4e92
FabiOliverr <bhalfur@gmail.com>
2025-09-08 13:21:43 -0400

    Adicionado novo módulo e primeiro exercicio

---
+++ Modulo 1-Git Hub e Versionamento/Exercicio-readme-git/README.md
@@ -0,0 +1,19 @@
+Abra seu arquivo README criado ao longo das aulas.
+OK
+
+Digite o e-mail que você utiliza na EBAC dentro deste arquivo README, modificando-o.
+OK
+
+Através do terminal git bash , utilize o comando git add para adicionar esta modificação.
+OK
+
+Utilize o comando git commit para marcar a modificação com uma mensagem.
+OK
+
+Utilize o comando git push para subir a alteração local para o repositório remoto.
+OK
+
+Por fim, compartilhe o link do seu repositório no campo entrega para que nossos tutores possam corrigi-la.
+OK
+
+Email-EBAC: bhalfur@gmail.com
---
+++ Modulo 2- Tipos de Dados./Variaveis-tipos-java/src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Main{
+    public static void main(String[] args){
+
+        //Modulo para entender as variáveis, que tem (numeros inteiros, numeros reais e o do tipo logico booleano)
+
+        //numeros inteiros e numeros reais
+        //exemplo números inteiros: 6,8,100
+        //exemplos de números reais 6.5,8.5,10.20 , -5,2
+
+        //tipos relacionados a numeros inteiros.
+        byte numero = 5;
+        short numero2 = 150;
+        int numero3 = 300;
+        long numero4 = 40000L;
+
+        //tipos relacionados a numeros reais.
+        float n1 = 8.5f;
+        double n2 = 7.5;
+
+        //tipos relacionados a letras.
+        char letra = 'A'; // Trabalha somente com 1 caractere.
+        String letra2 = "AB"; // Não é do tipo primitivo e trabalha com palavras.
+
+        //Verdadeiro ou falso.
+        boolean verdadeiroOuFalso = true; //Ele somente aceita True or False.
+        System.out.println(verdadeiroOuFalso);
+
+        verdadeiroOuFalso = false;
+
+        System.out.println(verdadeiroOuFalso);
+
+    }
+}
